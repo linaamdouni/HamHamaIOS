6b7c3c7440464f678b39f498cf1b44c63975071e
Mac <mac@macs-macbook-pro.local>
2019-04-07 10:22:47 +0100

    Initial Commit

---
+++ HamHamaIOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HamHamaIOS
+//
+//  Created by Mac on 4/7/19.
+//  Copyright © 2019 Mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
